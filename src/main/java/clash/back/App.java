7b7c56ff7847d55c43dbd15b23fd1b2106043899
merfanian <erfanianmahdi00@gmail.com>
2020-08-23 14:40:05 +0430

    Config project

--- src/main/java/clash/back/App.java
+++ src/main/java/clash/back/App.java
@@ -3,12 +3,14 @@
  */
 package clash.back;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
