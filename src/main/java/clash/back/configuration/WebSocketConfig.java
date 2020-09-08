1b3604b3198478dcca470a6ba03d7e011ce0d625
merfanian <erfanianmahdi00@gmail.com>
2020-09-08 13:31:35 +0430

    Modify sockJs config

--- src/main/java/clash/back/configuration/WebSocketConfig.java
+++ src/main/java/clash/back/configuration/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry
                 .addEndpoint("/websocket")
                 .setHandshakeHandler(new CustomHandshakeHandler())
-                .setAllowedOrigins("http://localhost:3000").withSockJS();
+                .setAllowedOrigins("*").withSockJS();
     }
 
     @Override
